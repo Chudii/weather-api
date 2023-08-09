8331014c799098183e02ff751ff6d7892811fff6
Chudi <chudiibida3@gmail.com>
2023-08-08 22:15:26 -0400

    tinkering weather units

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import Hourly from './components/hourly';
 
 const App = () => {
   const [weatherData, setWeatherData] = useState([])
@@ -14,7 +15,7 @@ const App = () => {
       setIsLoading(true)
 
       try {
-        const weatherResult = await axios("https://api.openweathermap.org/data/3.0/onecall?lat=40.735&lon=-74.172&appid=65d04fbbdd2d88832927515ecaae77b7")
+        const weatherResult = await axios("https://api.openweathermap.org/data/3.0/onecall?lat=40.735&lon=-74.172&units=imperial&appid=65d04fbbdd2d88832927515ecaae77b7")
 
         const cityResult = await axios("http://api.openweathermap.org/geo/1.0/direct?q=Newark&limit=1&appid=65d04fbbdd2d88832927515ecaae77b7")
 
@@ -55,6 +56,30 @@ const App = () => {
         <div className='day-forecast'>
           <p>TODAY'S FORECAST</p>
           <div className='day-times'>
+            <Hourly
+              label={'6:00 AM'}
+              temp={77}
+            />
+            <Hourly
+              label={'9:00 AM'}
+              temp={77}
+            />
+            <Hourly
+              label={'12:00 PM'}
+              temp={77}
+            />
+            <Hourly
+              label={'3:00 PM'}
+              temp={77}
+            />
+            <Hourly
+              label={'6:00 PM'}
+              temp={77}
+            />
+            <Hourly
+              label={'9:00 PM'}
+              temp={77}
+            />
           </div>
         </div>
         <div className='air-conditions'>

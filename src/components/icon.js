ff94f39b727b34ed642117f03237b01b17f95735
Chudi <chudiibida3@gmail.com>
2023-08-13 22:58:22 -0400

    added switch for icons

--- src/components/icon.js
+++ src/components/icon.js
@@ -11,9 +11,48 @@ import {
     WeatherAlert
   } from './assets/img/index.js'
 
-const Icon = ({ weather }) => {
+const Icon = ({ weather, currentTime, sunrise, sunset }) => {
+    const iconSelect = (weather) => {
+        switch (weather) {
+            case 'Fog':
+            case 'Mist':
+                return Cloudy
+            case 'Thunderstorm':
+                return Storm
+            case 'Drizzle':
+                return RainClouds
+            case 'Rain':
+                return RainCloudsPouring
+            case 'Snow':
+                return Snow
+            case 'Smoke':
+            case 'Haze':
+            case 'Dust':
+            case 'Sand':
+            case 'Ash':
+            case 'Squall':
+            case 'Tornado':
+                return WeatherAlert
+        }
+
+        if (currentTime >= sunrise) {
+            if (currentTime < sunset) {
+
+            } else {
+
+            }
+        } else {
+            
+        }
+    }
+
     return (
-        
+        <div>
+            <img 
+                className=''
+                src={iconSelect(weather)}
+            />
+        </div>
     )
 }
 

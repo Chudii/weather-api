bb3744af6e26012833c3c84556f0502fea1beb15
Chudi <chudiibida3@gmail.com>
2023-08-13 22:16:02 -0400

    added index to weather icons

---
+++ src/assets/img/index.js
@@ -0,0 +1,23 @@
+import Sunny from './sunny.png';
+import Moon from './moon.png';
+import Cloudy from './cloudy.png';
+import Snow from './snow.png';
+import Storm from './storm.png'
+import RainClouds from './rain-clouds';
+import PartlyCloudySun from './partly-cloudy-sun.png';
+import PartlyCloudyMoon from './partly-cloudy-moon.png';
+import RainCloudsPouring from './rain-clouds-pouring.png';
+import WeatherAlert from './weather-alert.png'
+
+export {
+    Sunny,
+    Moon,
+    Cloudy,
+    Snow,
+    Storm,
+    RainClouds,
+    PartlyCloudySun,
+    PartlyCloudyMoon,
+    RainCloudsPouring,
+    WeatherAlert
+};

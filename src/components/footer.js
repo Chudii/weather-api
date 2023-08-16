e7802be8420c8dd73999dcf967b2b9091d123921
Chudi <chudiibida3@gmail.com>
2023-08-16 19:55:10 -0400

    implemented footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,17 +3,14 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Footer = () => {
-
     return (
         <div className="footer">
-            <div className='social-media'>
-                <div className='icons'>
-                    <LinkedInIcon />
-                    <MailOutlineIcon />
-                    <GitHubIcon />
-                </div>
+            <div className='icons'>
+                <LinkedInIcon fontSize='large'/>
+                <MailOutlineIcon fontSize='large'/>
+                <GitHubIcon fontSize='large'/>
             </div>
-            <p>&copy; 2022 CodeNameChu</p>
+            <p>&copy; 2023 CodeNameChu</p>
         </div>
     )
 }

1d4ebc7640704bb052ff3f098419a9ccadf71a54
Chudi <chudiibida3@gmail.com>
2023-08-16 19:36:13 -0400

    added full mui icons package

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,6 @@
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Footer = () => {
 
@@ -6,7 +8,9 @@ const Footer = () => {
         <div className="footer">
             <div className='social-media'>
                 <div className='icons'>
-                    
+                    <LinkedInIcon />
+                    <MailOutlineIcon />
+                    <GitHubIcon />
                 </div>
             </div>
             <p>&copy; 2022 CodeNameChu</p>

890e7f380d351aa5042f66b24284047d1f395115
Chudi <chudiibida3@gmail.com>
2023-08-18 20:38:33 -0400

    tweaking styling for PC & desktops

--- src/App.js
+++ src/App.js
@@ -63,56 +63,60 @@ const App = () => {
         />
       </div>
 
-      <div className='current-day'>
-        {
-          weatherData.current &&
-          <div>
-            <div className='city-label'>
-              <h2 className='city-name'>{cityData.name}</h2>
-              <h3 className='city-state'>{cityData.state.toUpperCase()}</h3>
-            </div>
-            
-            <div className='calendar'>
-              <p>
-                {`${timestamp.day}, ${timestamp.month} ${timestamp.dayOfMonth}`}
-              </p>
-            </div>
+      <div className='info'>
+        <div className='current-day'>
+          {
+            weatherData.current &&
+            <div>
+              <div className='city-label'>
+                <h2 className='city-name'>{cityData.name}</h2>
+                <h3 className='city-state'>{cityData.state.toUpperCase()}</h3>
+              </div>
+              
+              <div className='calendar'>
+                <p>
+                  {`${timestamp.day}, ${timestamp.month} ${timestamp.dayOfMonth}`}
+                </p>
+              </div>
 
-            <div className='current-weather'>
-              <h1 id='current-temp'>{Math.round(weatherData.current.temp)} °</h1>
-              <Icon 
-                weather={weatherData.current.weather[0].main}
-                currentTime={weatherData.current.dt}
-                sunrise={weatherData.current.sunrise}
-                sunset={weatherData.current.sunset}
-              />
+              <div className='current-weather'>
+                <h1 id='current-temp'>{Math.round(weatherData.current.temp)} °</h1>
+                <Icon 
+                  weather={weatherData.current.weather[0].main}
+                  currentTime={weatherData.current.dt}
+                  sunrise={weatherData.current.sunrise}
+                  sunset={weatherData.current.sunset}
+                />
 
-              <p>Winds: {Math.round(weatherData.current.wind_speed)} mph</p>
-              <p>Precipitation: {weatherData.daily[0].pop * 100}%</p>
-              <p>Humidty: {weatherData.current.humidity}%</p>
-            </div>  
-            
-            
-          </div>
-        }  
+                <p>Winds: {Math.round(weatherData.current.wind_speed)} mph</p>
+                <p>Precipitation: {weatherData.daily[0].pop * 100}%</p>
+                <p>Humidty: {weatherData.current.humidity}%</p>
+              </div>  
+              
+              
+            </div>
+          }  
+        </div>
+
+        <div className='week'>
+          {
+            weatherData.daily &&
+            weatherData.daily.slice(1).map((x, i) => {
+              return (
+                <Daily
+                  key={i}
+                  weather={weatherData.daily[i+1].weather[0].main}
+                  time={weatherData.daily[i+1].dt}
+                  tempHi={weatherData.daily[i+1].temp.max}
+                  tempLo={weatherData.daily[i+1].temp.min}
+                />
+              )
+            })
+          }
+        </div>
       </div>
 
-      <div className='week'>
-        {
-          weatherData.daily &&
-          weatherData.daily.slice(1).map((x, i) => {
-            return (
-              <Daily
-                key={i}
-                weather={weatherData.daily[i+1].weather[0].main}
-                time={weatherData.daily[i+1].dt}
-                tempHi={weatherData.daily[i+1].temp.max}
-                tempLo={weatherData.daily[i+1].temp.min}
-              />
-            )
-          })
-        }
-      </div>
+      
 
       <Footer />
     </div>
